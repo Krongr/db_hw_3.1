269f515c52788b8aec2362b713cd1225863868b4
Artem Nazarov <94957939+Krongr@users.noreply.github.com>
2022-02-14 15:32:15 +0300

    minor fixes

--- netology_hw_3.1_script.sql
+++ netology_hw_3.1_script.sql
@@ -10,20 +10,20 @@ create table if not exists genre_list (
 );
 
 create table if not exists singer_genre_pair (
-	singer_id integer references singer_list(id),
-	genre_id integer references genre_list(id),
+	singer_id integer references singer_list(id) not null,
+	genre_id integer references genre_list(id) not null,
 	constraint singer_genre_pk primary key (singer_id, genre_id)
 );
 
 create table if not exists record_album_list (
 	id serial primary key,
 	album_name varchar(40) not null unique,
-	release_year integer  not null 
+	release_year integer not null 
 );
 
 create table if not exists singer_album_pair (
-	singer_id integer references singer_list(id),
-	album_id integer references record_album_list(id),
+	singer_id integer references singer_list(id) not null,
+	album_id integer references record_album_list(id) not null,
 	constraint singer_album_pk primary key (singer_id, album_id)
 );
 
@@ -31,7 +31,7 @@ create table if not exists track_list (
 	id serial primary key,
 	track_name varchar(40) not null unique,
 	track_length integer not null,
-	album_id integer references record_album_list(id)
+	album_id integer references record_album_list(id) not null
 );
 
 create table if not exists music_collection_list (
@@ -41,7 +41,7 @@ create table if not exists music_collection_list (
 );
 
 create table if not exists track_collection_pair (
-	track_id integer references track_list(id),
-	collection_id integer references music_collection_list(id),
+	track_id integer references track_list(id) not null,
+	collection_id integer references music_collection_list(id) not null,
 	constraint track_collection_pk primary key (track_id, collection_id)
 );
